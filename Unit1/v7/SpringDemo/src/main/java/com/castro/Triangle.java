d4e43b73125c4d2eb26221e320015e56aada65fd
Glenn Castro <glenn.castro@gmail.com>
2015-01-21 10:27:10 +1100

    Unit1 - Version 7 - Bean Autowiring

---
+++ Unit1/v7/SpringDemo/src/main/java/com/castro/Triangle.java
@@ -0,0 +1,40 @@
+package com.castro;
+
+import java.util.List;
+
+public class Triangle {
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	public void draw() {
+		System.out.printf("Point A = (%s, %s)%n", getPointA().getX(), getPointA().getY());
+		System.out.printf("Point B = (%s, %s)%n", getPointB().getX(), getPointB().getY());
+		System.out.printf("Point C = (%s, %s)%n", getPointC().getX(), getPointC().getY());
+	}
+}
