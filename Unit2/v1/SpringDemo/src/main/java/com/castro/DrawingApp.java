662236595998b411a90151b08b192a091ddd0d31
Glenn Castro <glenn.castro@gmail.com>
2015-01-22 23:35:04 +1100

    Unit2 - Version 1 - Bean Scopes
    Singleton, Prototype, Request, Session, Global Session

---
+++ Unit2/v1/SpringDemo/src/main/java/com/castro/DrawingApp.java
@@ -0,0 +1,18 @@
+package com.castro;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+		triangle.getPointA().setX(1);
+		triangle.getPointA().setY(1);
+		
+		Triangle t2 = (Triangle) context.getBean("triangle");
+		t2.draw();
+	}
+}
